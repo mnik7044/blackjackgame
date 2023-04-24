8739a5fb2030dd218d2650263d5d68e3442e7d4e
Nikhil Mishra <108953684+mnik7044@users.noreply.github.com>
2023-04-24 07:59:34 +0530

    Done with styling buttons and background

---
+++ intro.js
@@ -0,0 +1,34 @@
+let firstCard=10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = " "
+
+let messageEl = document.getElementById("message-el")
+
+let sumEl = document.getElementById("sum-el")
+
+console.log(sumEl)
+
+function startGame(){
+
+sumEl.textContent = "Sum: " + sum
+if(sum<=20)
+{
+    message = "Do you still wanna draw"
+}
+else if(sum ===21) 
+{
+    message = "You won a blackjack"
+    hasBlackJack = true;
+
+}
+else{
+    message = "You are out sorry!"
+    isAlive= false
+}
+messageEl.textContent = message
+}
+//Cash Out 
+
