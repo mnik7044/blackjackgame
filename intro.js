7e41cf96db069c154a463c99368b8b157c420cab
Nikhil Mishra <108953684+mnik7044@users.noreply.github.com>
2023-04-27 03:57:31 +0530

    Added randomness

--- intro.js
+++ intro.js
@@ -1,5 +1,5 @@
-let firstCard=10
-let secondCard = 4
+let firstCard=getRandomCard()
+let secondCard = getRandomCard()
 let cards=[firstCard,secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -11,6 +11,11 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 
 let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard(){
+    return  Math.floor( Math.random()*6 ) + 1
+
+}
 
 console.log(sumEl)
 
@@ -44,7 +49,7 @@ else{
 messageEl.textContent = message
 }
 function newCard(){
-    let card = 7
+    let card = getRandomCard()
     sum+= card
     cards.push(card)
     renderGame()
